8e472a9a734ee5424fa96e310f0f1510560b6427
Kaiiler <Kaiiler@live.se>
2017-08-29 11:00:58 +0200

    added test for button functionality

--- src/app/calculator/calculator.component.spec.ts
+++ src/app/calculator/calculator.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CalculatorComponent } from './calculator.component';
+import { By } from '@angular/platform-browser';
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
@@ -18,8 +18,16 @@ describe('CalculatorComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+  it('should add the clicked number to the display', () => {
+    const el = fixture.debugElement.queryAll(By.css('button'));
+    const screen = fixture.debugElement.queryAll(By.css('.result'));
+
+    el[1].nativeElement.click();
+    fixture.detectChanges();
+
+    expect(screen[0].nativeElement.innerHTML).toContain(el[1].nativeElement.innerHTML);
   });
 });
