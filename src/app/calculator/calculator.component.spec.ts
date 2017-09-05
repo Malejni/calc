ce8355239c347e7c8ceccce03156929a2503c826
Kaiiler <Kaiiler@live.se>
2017-09-05 11:23:29 +0200

    added tests

--- src/app/calculator/calculator.component.spec.ts
+++ src/app/calculator/calculator.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { CalculatorComponent } from './calculator.component';
 
@@ -21,5 +22,63 @@ describe('CalculatorComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should enter number', () => {
+    const btn = fixture.debugElement.queryAll(By.css('button'));
+    const screen = fixture.debugElement.query(By.css('div'));
+    btn[0].nativeElement.click();
+    fixture.detectChanges();
+    expect(screen.nativeElement.innerHTML).toContain(btn[0].nativeElement.innerHTML);
+  });
+  it('should calculate according to divide', () => {
+    const btn = fixture.debugElement.queryAll(By.css('button'));
+    const screen = fixture.debugElement.query(By.css('div'));
+    btn[0].nativeElement.click();
+    btn[3].nativeElement.click();
+    btn[0].nativeElement.click();
+    btn[14].nativeElement.click();
+    fixture.detectChanges();
+    expect(screen.nativeElement.innerHTML).toBe('Result:1');
+  });
+  it('should calculate according to mutliply', () => {
+    const btn = fixture.debugElement.queryAll(By.css('button'));
+    const screen = fixture.debugElement.query(By.css('div'));
+    btn[0].nativeElement.click();
+    btn[7].nativeElement.click();
+    btn[0].nativeElement.click();
+    btn[14].nativeElement.click();
+    fixture.detectChanges();
+    expect(screen.nativeElement.innerHTML).toBe('Result:49');
+  });
+  it('should calculate according to plus', () => {
+    const btn = fixture.debugElement.queryAll(By.css('button'));
+    const screen = fixture.debugElement.query(By.css('div'));
+    btn[0].nativeElement.click();
+    btn[15].nativeElement.click();
+    btn[0].nativeElement.click();
+    btn[14].nativeElement.click();
+    fixture.detectChanges();
+    expect(screen.nativeElement.innerHTML).toBe('Result:14');
+  });
+  it('should calculate according to minus', () => {
+    const btn = fixture.debugElement.queryAll(By.css('button'));
+    const screen = fixture.debugElement.query(By.css('div'));
+    btn[0].nativeElement.click();
+    btn[11].nativeElement.click();
+    btn[0].nativeElement.click();
+    btn[14].nativeElement.click();
+    fixture.detectChanges();
+    expect(screen.nativeElement.innerHTML).toBe('Result:0');
+  });
+  it('should calculate according to modulus', () => {
+    const btn = fixture.debugElement.queryAll(By.css('button'));
+    const screen = fixture.debugElement.query(By.css('div'));
+    btn[0].nativeElement.click();
+    btn[19].nativeElement.click();
+    btn[0].nativeElement.click();
+    btn[14].nativeElement.click();
+    fixture.detectChanges();
+    expect(screen.nativeElement.innerHTML).toBe('Result:0');
   });
 });
