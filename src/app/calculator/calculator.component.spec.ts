d5cab7f47e7b5b585c63a1575e01d3edc09f3311
Kaiiler <Kaiiler@live.se>
2017-09-05 12:32:25 +0200

    lets try again..

--- src/app/calculator/calculator.component.spec.ts
+++ src/app/calculator/calculator.component.spec.ts
@@ -79,7 +79,6 @@ describe('CalculatorComponent', () => {
     btn[0].nativeElement.click();
     btn[14].nativeElement.click();
     fixture.detectChanges();
-
     expect(screen.nativeElement.innerHTML).toBe('Result:0');
   });
 });
